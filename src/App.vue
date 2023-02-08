fae69050dcf5a07ac79d26e93ad644c5a36ca27c
Leobardo D. Martinez-Quiroz <ldmartinezquiroz@gmail.com>
2023-02-08 12:06:45 -0500

    dark mode added

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,79 @@
 <template>
+  <div class="app" :class="mode">
+  <Header :mode="mode" @toggle="toggle" />
+  
+  </div>
+
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
+  
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from '@/components/Header.vue'
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'app',
+  data () {
+    return {
+      mode: 'light'
     }
+  },
+  components: {
+    Header,
+  },
+  methods: {
+    toggle () {
+      if (this.mode === "dark") {
+        this.mode = "light"
+      } else {
+        this.mode = "dark"
+      }
+    }
+
   }
 }
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto', sans-serif;   // Need to change to Montserrat
+}
+.app {
+  width: 100vw;
+  min-height: 100vh;
+  background: #F3F3F3;
+  color: #15202B;
+  transition: background 0.3s ease-in-out;
+}
+.dark {
+  background: #192734;
+  color: #F3F3F3;
+}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+// nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+
+<togglertest/>
+
   </div>
 </template>
+
+<script>
+import togglertest from '../components/togglertest.vue'
+export default {
+  components: {
+    togglertest,
+  }
+
+}
+</script>
+
+<style>
+
+</style>
